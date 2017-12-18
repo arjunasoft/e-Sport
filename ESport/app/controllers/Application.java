360b82ca4b8d6d2edb019b499bf154b0efcd3728
ahmadnadzirr <ahmadnadzirromadhon@gmail.com>
2017-12-18 08:18:05 -0800

    Penambahan app

--- ESport/app/controllers/Application.java
+++ ESport/app/controllers/Application.java
@@ -1,14 +1,38 @@
 package controllers;
 
-import play.*;
 import play.mvc.*;
 
-import views.html.*;
+import play.data.validation.*;
 
+import java.util.*;
+
+import models.*;
+	
 public class Application extends Controller {
+	
+    public static void index() {
+    	Date now = new Date();
+        render(now);
+    }
+    public static void list() {
+        List<user> User1 = user.find("order by Lokasi").fetch();
+        render(User1);
+    }
+        public static void form(Long id) {
+            if(id == null) {
+                render();
+    }
+        User User2 = User.findById(id);
+        render(User2);
+   }
+        public static void save(@Valid data_pengguna data_pengguna) {
+            if(validation.hasErrors()) {
+                if(request.isAjax()) error("Invalid value");
+                render("@form", User);
+            }
+            User.save();
+            list();
+        }
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
     }
 
-}
